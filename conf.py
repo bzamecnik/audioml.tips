cd74ca742da6ca33a645a246f31f4ede1ed4f696
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2016-10-17 18:32:09 +0200

    Make markdown the default format for new posts.

--- conf.py
+++ conf.py
@@ -180,14 +180,14 @@ THEME_COLOR = '#5670d4'
 #
 
 POSTS = (
-    ("posts/*.rst", "posts", "post.tmpl"),
     ("posts/*.md", "posts", "post.tmpl"),
+    ("posts/*.rst", "posts", "post.tmpl"),
     ("posts/*.txt", "posts", "post.tmpl"),
     ("posts/*.ipynb", "posts", "post.tmpl"),
 )
 PAGES = (
-    ("stories/*.rst", "stories", "story.tmpl"),
     ("stories/*.md", "stories", "story.tmpl"),
+    ("stories/*.rst", "stories", "story.tmpl"),
     ("stories/*.txt", "stories", "story.tmpl"),
     ("stories/*.ipynb", "stories", "story.tmpl"),
 )
